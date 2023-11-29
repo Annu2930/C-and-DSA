f24da6f792bf1bb76a7b41c5a186dddbd8462645
Anamika <118174318+Annu2930@users.noreply.github.com>
2023-11-29 23:51:59 +0530

    Assign_29.c

--- MySirg_C/C_Lang_MYsirg/Arrays_Assignments/Assign_29.c
+++ MySirg_C/C_Lang_MYsirg/Arrays_Assignments/Assign_29.c
@@ -19,27 +19,24 @@ int q5(){
 
   return 0;
 }
-
+ 
 //qn4.wap to calculate a largest numbers stored in array size of 10.Take array value from users 
-/*int main(){
-   int arr[10];
-  printf("Enter the 10 number: ");
-  for (int i = 0; i <= 9; ++i) {
-    printf("Enter number %d\n: ", i + 1);
-    scanf("%d", &arr[i]);
-  }
+int main(){
+   int a[10],i,max;
+  printf("Enter the 10 numbers: ");
+  for (i = 0; i <= 9; i++) 
+    scanf("%d", &a[i]);
+    max=a[0];
+  for (i = 0; i <= 9;i++)
+    if (max < a[i]) 
+      max = a[i];
+    
 
-  // storing the largest number to arr[0]
-  for (int i = 0; i <= 9; ++i) {
-    if (arr[0] < arr[i]) {
-      arr[0] = arr[i];
-    }
-  }
-
-  printf("Largest element = %d", arr[0]);
+  printf("Largest number of array is %d", max);
 
   return 0;
-}*/
+}
+
 //wap to calculate sum of all even and odd numebrs. which are sotred in array size 10. take value from users
 int main(){
     int a[10],i,sumEven=0,sumOdd=0;
